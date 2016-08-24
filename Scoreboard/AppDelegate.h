eed5b5c96cca37ed2ebbf5c496a1c9f4fd750b85
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-08-24 15:12:58 -0400

    Initial Commit

---
+++ Scoreboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scoreboard
+//
+//  Created by Daniel Kwolek on 8/24/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scoreboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scoreboard
+//
+//  Created by Daniel Kwolek on 8/24/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
